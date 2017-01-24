11547d5b49455b386cff34bad89de4ef52dc512e
tasconajoey <joey.tascona@gmail.com>
2017-01-24 13:11:50 -0500

    Add content to my contact.erb file

--- crm.rb
+++ crm.rb
@@ -2,6 +2,11 @@
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
 require 'sinatra'
+require_relative 'contact'
+
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 get '/' do
   @crm_app_name = "Joey's CRM"
