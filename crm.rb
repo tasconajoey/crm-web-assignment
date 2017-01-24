abf5dd4d5e119bf84893a40f7d446402deb6c84b
tasconajoey <joey.tascona@gmail.com>
2017-01-24 13:39:28 -0500

    Finish assignment

--- crm.rb
+++ crm.rb
@@ -4,10 +4,6 @@
 require 'sinatra'
 require_relative 'contact'
 
-Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
-
 get '/' do
   @crm_app_name = "Joey's CRM"
   erb :index
@@ -19,4 +15,9 @@ end
 
 get '/contacts/new' do
   erb :new_contacts
+end
+
+post '/contacts' do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
 end
