e497969953db2d7df096b872d66038efa0f73dc9
tasconajoey <joey.tascona@gmail.com>
2017-01-26 12:29:46 -0500

    Finish second part of assignment

--- crm.rb
+++ crm.rb
@@ -23,7 +23,12 @@ get '/contacts/new' do
 end
 
 post '/contacts' do
-  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  contact = Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
   redirect to('/contacts')
 end
 
